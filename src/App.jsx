d974bb2f2cdda03d75a897beab33ca57f8b83889
Deep Singh <deepsingh25244@gmail.com>
2023-06-22 00:24:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/FVBx0Ri4OsqCYBBa/scene.splinecode" />
+  );
+}

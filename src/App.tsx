3d569019c7d93efd7951c348858d735da5850396
deepsingh245 <deepsingh25244@gmail.com>
2025-05-24 00:13:28 +0530

    fixes: breaking changes - folder sructure changed - shadcn installed - ts introduced - code updated

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from '@/router';
+import Navbar from '@/components/layout/Navbar';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <AppRoutes />
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
 const root = createRoot(rootElement);
+
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
